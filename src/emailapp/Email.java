8851f4f50d32d23d96a5677fec561fa01bb063f0
Hara <harachung90@gmail.com>
2022-08-13 20:52:36 +1000

    Initial commit. Set the constructor.

---
+++ src/emailapp/Email.java
@@ -0,0 +1,33 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //constructor to receive the first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created: " + this.firstName + " " + this.lastName);
+    }
+
+    //ask for the department
+
+
+    //generate a random password
+
+
+    //set the mailbox capacity
+
+
+    //set the alternate email
+
+
+    //change the password
+
+
+}
